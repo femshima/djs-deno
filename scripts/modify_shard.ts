a7db594ed9304ea3f4dd95576f1fdf0105a8536d
femshima <49227365+femshima@users.noreply.github.com>
2023-01-25 06:37:02 +0900

    `deno fmt`

--- scripts/modify_shard.ts
+++ scripts/modify_shard.ts
@@ -1,15 +1,15 @@
-const path = 'node_modules/discord.js/src/client/websocket/WebSocketShard.js';
+const path = "node_modules/discord.js/src/client/websocket/WebSocketShard.js";
 
 const orig = await Deno.readTextFile(path);
 
 const modified = orig
   .replace(
     "const WebSocket = require('../../WebSocket');",
-    "let WebSocket = require('../../WebSocket');"
+    "let WebSocket = require('../../WebSocket');",
   )
   .replace(
-    'class WebSocketShard extends EventEmitter {',
-    'class WebSocketShard extends EventEmitter {\nstatic setWS(ws){WebSocket=ws;}'
-  )
+    "class WebSocketShard extends EventEmitter {\n    ",
+    "class WebSocketShard extends EventEmitter {\nstatic setWS(ws){WebSocket=ws;}",
+  );
 
 await Deno.writeTextFile(path, modified);
--- ws.ts
+++ ws.ts
@@ -1,72 +1,75 @@
 import { EventEmitter } from "https://deno.land/std@0.161.0/node/events.ts";
 
-type Listener<K extends keyof WebSocketEventMap> = (args: WebSocketEventMap[K]) => void | Promise<void>;
+type Listener<K extends keyof WebSocketEventMap> = (
+  args: WebSocketEventMap[K],
+) => void | Promise<void>;
 
 class MyWebSocket extends EventEmitter {
   static CONNECTING = 0;
   static OPEN = 1;
   static CLOSING = 2;
   static CLOSED = 3;
-  static encoding = 'json';
+  static encoding = "json";
   static create(url: string) {
     return new MyWebSocket(url);
   }
-  static pack = JSON.stringify
+  static pack = JSON.stringify;
   static unpack(data: string | BufferSource) {
     const ab = new TextDecoder();
-    if (typeof data !== 'string') {
+    if (typeof data !== "string") {
       data = ab.decode(data);
     }
     return JSON.parse(data);
   }
 
   #listeners: {
-    [K in keyof WebSocketEventMap]?: Listener<K>
+    [K in keyof WebSocketEventMap]?: Listener<K>;
   } = {};
-  #socket: WebSocket
+  #socket: WebSocket;
   constructor(url: string, protocols: string[] = []) {
-    super()
-    this.#socket = new WebSocket(url, protocols)
+    super();
+    this.#socket = new WebSocket(url, protocols);
 
-    this.on('open', args => this.#listeners.open?.(args));
-    this.on('message', args => this.#listeners.message?.(args));
-    this.on('error', args => this.#listeners.error?.(args));
-    this.on('close', args => this.#listeners.close?.(args));
+    this.on("open", (args) => this.#listeners.open?.(args));
+    this.on("message", (args) => this.#listeners.message?.(args));
+    this.on("error", (args) => this.#listeners.error?.(args));
+    this.on("close", (args) => this.#listeners.close?.(args));
   }
-  override on<K extends keyof WebSocketEventMap>(event: K, listener: Listener<K>): this {
-    super.on(event, listener)
+  override on<K extends keyof WebSocketEventMap>(
+    event: K,
+    listener: Listener<K>,
+  ): this {
+    super.on(event, listener);
     this.#socket.addEventListener(event, (ev) => {
-      super.emit(event, ev)
-    })
-    return this
+      super.emit(event, ev);
+    });
+    return this;
   }
   get readyState(): number {
-    return this.#socket.readyState
+    return this.#socket.readyState;
   }
   send(content: string | ArrayBufferLike | Blob | ArrayBufferView) {
-    return this.#socket.send(content)
+    return this.#socket.send(content);
   }
   close(code?: number, reason?: string) {
-    return this.#socket.close(code, reason)
+    return this.#socket.close(code, reason);
   }
   terminate() {
-    this.#socket.close()
+    this.#socket.close();
   }
 
-  set onopen(listener: Listener<'open'> | null) {
+  set onopen(listener: Listener<"open"> | null) {
     this.#listeners.open = listener ?? undefined;
   }
-  set onmessage(listener: Listener<'message'> | null) {
+  set onmessage(listener: Listener<"message"> | null) {
     this.#listeners.message = listener ?? undefined;
   }
-  set onerror(listener: Listener<'error'> | null) {
+  set onerror(listener: Listener<"error"> | null) {
     this.#listeners.error = listener ?? undefined;
   }
-  set onclose(listener: Listener<'close'> | null) {
+  set onclose(listener: Listener<"close"> | null) {
     this.#listeners.close = listener ?? undefined;
   }
 }
 
-export {
-  MyWebSocket
-}
+export { MyWebSocket };
